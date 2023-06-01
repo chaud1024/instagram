dba36506a8be265569273de6813d5d861ca29565
chaud1024 <chaud1024@gmail.com>
2023-06-01 16:41:06 +0900

    #12-17 로그인버튼 size props에 따른 크기 조절 기능 추가

--- src/app/components/ui/ColorButton.tsx
+++ src/app/components/ui/ColorButton.tsx
@@ -2,14 +2,20 @@ import React from "react";
 type Props = {
   text: string;
   onClick: () => void;
+  size: "small" | "big";
 };
 
-export default function ColorButton({ text, onClick }: Props) {
+export default function ColorButton({ text, onClick, size = "small" }: Props) {
   return (
     <button
       onClick={onClick}
-      className="rounded-lg bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-300 py-0.5 px-0.5">
-      <span className="bg-white inline-block py-0.5 px-1.5 rounded-md line hover:opacity-80 transition-opacity">
+      className={`rounded-lg bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-300 py-0.5 px-0.5
+        ${size === "big" ? "p-[0.3rem]" : "p-[0.15rem]"}
+      `}>
+      <span
+        className={`bg-white inline-block rounded-md line hover:opacity-80 transition-opacity
+          ${size === "big" ? "p-4 text-2xl" : "p-[0.3rem] text-base"}
+        `}>
         {text}
       </span>
     </button>
