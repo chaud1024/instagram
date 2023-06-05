e6c0e86658953e2123fc50cceda7aa6c00a3ecf0
chaud1024 <chaud1024@gmail.com>
2023-06-05 16:16:02 +0900

    #13-4 버튼 css 수정

--- src/app/components/ui/ColorButton.tsx
+++ src/app/components/ui/ColorButton.tsx
@@ -9,12 +9,12 @@ export default function ColorButton({ text, onClick, size = "small" }: Props) {
   return (
     <button
       onClick={onClick}
-      className={`rounded-lg bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-300 py-0.5 px-0.5
+      className={`rounded-lg bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-300 
         ${size === "big" ? "p-[0.3rem]" : "p-[0.15rem]"}
       `}>
       <span
         className={`bg-white inline-block rounded-md line hover:opacity-80 transition-opacity
-          ${size === "big" ? "p-4 text-2xl" : "p-[0.3rem] text-base"}
+          ${size === "big" ? "p-4 text-2xl" : "p-[0.4rem] text-base"}
         `}>
         {text}
       </span>
