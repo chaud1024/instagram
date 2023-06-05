12c67fed5655f8023f021631a671833b8158d4a9
chaud1024 <chaud1024@gmail.com>
2023-06-05 16:18:04 +0900

    #13-4 메인화면 생성한 컴포넌트 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
-"use client";
-import { usePathname } from "next/navigation";
+import FollowingBar from "./components/FollowingBar";
+import PostList from "./components/PostList";
+import SideBar from "./components/SideBar";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import { redirect } from "next/navigation";
 
-export default function Home() {
-  const pathname = usePathname();
-  return <h1>{pathname}</h1>;
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+  const user = session?.user;
+
+  if (!user) {
+    redirect("/auth/signin");
+  }
+
+  return (
+    <section className="flex flex-col md:flex-row w-full max-w-[850px] p-4">
+      <div className="w-full basis-3/4">
+        <FollowingBar />
+        <PostList />
+      </div>
+      <div className="basis-1/4">
+        <SideBar user={user} />
+      </div>
+    </section>
+  );
 }
