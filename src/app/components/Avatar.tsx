b599cad5109034108e7aa83f58141b26b6d07e9e
chaud1024 <chaud1024@gmail.com>
2023-06-05 16:16:35 +0900

    #13-4 Avatar 컴포넌트 재사용성 향상: Props로 스타일링

--- src/app/components/Avatar.tsx
+++ src/app/components/Avatar.tsx
@@ -2,18 +2,39 @@ import React from "react";
 
 type Props = {
   image?: string | null;
+  size?: "small" | "normal";
+  hightlight?: boolean;
 };
 
-export default function Avatar({ image }: Props) {
+export default function Avatar({
+  image,
+  size = "normal",
+  hightlight = false,
+}: Props) {
   return (
-    <div className="rounded-full bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-300 py-0.5 px-0.5 w-9 h-9">
+    <div className={getContainerStyle(size, hightlight)}>
       {/* eslint-disable-next-line @next/next/no-img-element */}
       <img
         src={image ?? undefined}
         alt="user profile"
-        className="rounded-full p-[0.1rem]"
+        className={`rounded-full bg-white ${getImageSizeStyle(size)}`}
         referrerPolicy="no-referrer"
       />
     </div>
   );
+}
+
+function getContainerStyle(size: string, hightlight: boolean): string {
+  const baseStyle = "rounded-full flex justify-center items-center"; //  py-0.5 px-0.5 '
+  const hightlightStyle = hightlight
+    ? "bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-300"
+    : "";
+  const sizeStyle = size === "small" ? "w-9 h-9" : "w-[66px] h-[66px]";
+  return `${baseStyle} ${hightlightStyle} ${sizeStyle}`;
+}
+
+function getImageSizeStyle(size: string): string {
+  return size === "small"
+    ? "w-[34px] h-[34px]  p-[0.1rem]"
+    : "w-16 h-16  p-[0.2rem]";
 }
