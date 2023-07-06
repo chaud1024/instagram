731f54a4bec4c1c5a5bf1bb7ef5213b4b4730073
chaud1024 <chaud1024@gmail.com>
2023-07-06 17:32:15 +0900

    edit: add react-spinner, ScrollableBar component

--- src/app/components/FollowingBar.tsx
+++ src/app/components/FollowingBar.tsx
@@ -1,15 +1,20 @@
 "use client";
+import Link from "next/link";
+import { PropagateLoader } from "react-spinners";
 import useSWR from "swr";
 import { DetailUser } from "../model/user";
-import { PropagateLoader } from "react-spinners";
-import Link from "next/link";
 import Avatar from "./Avatar";
+import ScrollableBar from "./ScrollableBar";
 
 export default function FollowingBar() {
   const { data, isLoading: loading, error } = useSWR<DetailUser>("/api/me");
   // console.log(data?.following);
 
-  const users = data?.following;
+  const users = data?.following && [
+    ...data?.following,
+    ...data?.following,
+    ...data?.following,
+  ];
 
   return (
     <section className="w-full flex justify-center items-center p-4 shadow-sm shadow-neutral-300 mb-4 rounded-lg min-h-[90px] overflow-x-auto">
@@ -19,20 +24,19 @@ export default function FollowingBar() {
         (!users || users.length === 0) && <p>{`You don't have following.`}</p>
       )}
       {users && users.length > 0 && (
-        <ul className="w-full flex gap-2">
+        <ScrollableBar>
           {users.map(({ image, username }) => (
-            <li key={username}>
-              <Link
-                href={`/user/${username}`}
-                className="w-20 flex flex-col items-center">
-                <Avatar image={image} hightlight />
-                <p className="w-full text-sm text-ellipsis overflow-hidden text-center">
-                  {username}
-                </p>
-              </Link>
-            </li>
+            <Link
+              key={username}
+              href={`/user/${username}`}
+              className="w-20 flex flex-col items-center">
+              <Avatar image={image} hightlight />
+              <p className="w-full text-sm text-ellipsis overflow-hidden text-center">
+                {username}
+              </p>
+            </Link>
           ))}
-        </ul>
+        </ScrollableBar>
       )}
     </section>
   );
