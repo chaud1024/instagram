648ece23dfa49712a46922d013fde6c60fc158e0
chaud1024 <chaud1024@gmail.com>
2023-07-07 14:15:12 +0900

    edit: import post list card component into post list

--- src/app/components/PostList.tsx
+++ src/app/components/PostList.tsx
@@ -1,12 +1,29 @@
 "use client";
+import { GridLoader } from "react-spinners";
 import useSWR from "swr";
 import { SimplePost } from "../model/post";
+import PostListCard from "./PostListCard";
 
 export default function PostList() {
   const { data: posts, isLoading: loading } =
     useSWR<SimplePost[]>("/api/posts");
-  // console.log(data);
+  // console.log(posts);
   return (
-    <ul>{posts && posts.map((post) => <li key={post.id}>{post.text}</li>)}</ul>
+    <section>
+      {loading && (
+        <div className="text-center mt-32">
+          <GridLoader color="red" />
+        </div>
+      )}
+      {posts && (
+        <ul>
+          {posts.map((post) => (
+            <li key={post.id} className="mb-4">
+              <PostListCard post={post} />
+            </li>
+          ))}
+        </ul>
+      )}
+    </section>
   );
 }
