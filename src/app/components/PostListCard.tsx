424b4b3c7075a46b67ea61f16516908cbb139886
chaud1024 <chaud1024@gmail.com>
2023-07-10 15:17:53 +0900

    #13-21 포스트 상세 다이얼로그 포스트리스트카드 컴포넌트에 추가

--- src/app/components/PostListCard.tsx
+++ src/app/components/PostListCard.tsx
@@ -3,10 +3,11 @@ import Image from "next/image";
 import { useState } from "react";
 import { SimplePost } from "../model/post";
 import ActionBar from "./ActionBar";
-import Avatar from "./Avatar";
 import CommentForm from "./CommentForm";
-import PostModal from "./PostModal";
+import PostUserAvatar from "./PostUserAvatar";
 import ModalPortal from "./ui/ModalPortal";
+import PostModal from "./PostModal";
+import PostDetail from "./PostDetail";
 
 type Props = {
   post: SimplePost;
@@ -18,10 +19,7 @@ export default function PostListCard({ post, priority = false }: Props) {
   const [openModal, setOpenModal] = useState(false);
   return (
     <article className="rounded-lg shadow-md border-gray-200">
-      <div className="flex items-center p-2">
-        <Avatar image={userImage} highlight size="medium" />
-        <span className="text-gray-900 font-bold ml-2">{username}</span>
-      </div>
+      <PostUserAvatar image={userImage} username={username} />
       <Image
         className="w-full object-cover aspect-square"
         src={image}
@@ -41,7 +39,7 @@ export default function PostListCard({ post, priority = false }: Props) {
       {openModal && (
         <ModalPortal>
           <PostModal onClose={() => setOpenModal(false)}>
-            <p>포스트 상세 페이지</p>
+            <PostDetail post={post} />
           </PostModal>
         </ModalPortal>
       )}
