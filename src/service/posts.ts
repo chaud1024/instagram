eda152bf540bc836dafab0789ef43fb8f72e187b
chaud1024 <chaud1024@gmail.com>
2023-07-11 16:54:51 +0900

    #13-33 사용자 포스트 sanity service 추가

--- src/service/posts.ts
+++ src/service/posts.ts
@@ -10,7 +10,7 @@ const simplePostProjection = `
     "text": comments[0].comment,
     "comments": count(comments),
     "id":_id,
-    "createdAt":_createdAt 
+    "createdAt":_createdAt
 `;
 
 export async function getFollowingPostsOf(username: string) {
@@ -20,9 +20,7 @@ export async function getFollowingPostsOf(username: string) {
     || author._ref in *[_type == "user" && username == "${username}"].following[]._ref]
     | order(_createdAt desc){${simplePostProjection}}`,
     )
-    .then((posts) =>
-      posts.map((post: SimplePost) => ({ ...post, image: urlFor(post.image) })),
-    );
+    .then(mapPosts);
 }
 
 export async function getPost(id: string) {
@@ -40,4 +38,49 @@ export async function getPost(id: string) {
         }`,
     )
     .then((post) => ({ ...post, image: urlFor(post.image) }));
+}
+
+export async function getPostsOf(username: string) {
+  return client
+    .fetch(
+      `
+    *[_type == "post" && author->username == "${username}"]
+    | order(_createdAt desc){
+      ${simplePostProjection}
+    }
+    `,
+    )
+    .then(mapPosts);
+}
+
+export async function getLikedPostsOf(username: string) {
+  return client
+    .fetch(
+      `
+    *[_type == "post" && "${username}" in likes[]->username]
+    | order(_createdAt desc){
+      ${simplePostProjection}
+    }
+    `,
+    )
+    .then(mapPosts);
+}
+export async function getSavedPostsOf(username: string) {
+  return client
+    .fetch(
+      `
+    *[_type == "post" && _id in *[_type=="user" && username=="${username}"].bookmarks[]._ref]
+    | order(_createdAt desc){
+      ${simplePostProjection}
+    }
+    `,
+    )
+    .then(mapPosts);
+}
+
+function mapPosts(posts: SimplePost[]) {
+  return posts.map((post: SimplePost) => ({
+    ...post,
+    image: urlFor(post.image),
+  }));
 }
