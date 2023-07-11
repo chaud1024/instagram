07eec9361c3a857f05b8c601464714848e39dd19
chaud1024 <chaud1024@gmail.com>
2023-07-11 16:57:05 +0900

    #13-33 사용자 상세화면에 포스트 컴포넌트 추가

--- src/app/user/[username]/page.tsx
+++ src/app/user/[username]/page.tsx
@@ -1,3 +1,4 @@
+import UserPosts from "@/app/components/UserPosts";
 import UserProfile from "@/app/components/UserProfile";
 import { getUserForProfile } from "@/service/user";
 import { notFound } from "next/navigation";
@@ -17,6 +18,7 @@ export default async function UserPage({ params: { username } }: Props) {
   return (
     <>
       <UserProfile user={user} />
+      <UserPosts user={user} />
     </>
   );
 }
