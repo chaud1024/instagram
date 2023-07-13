d3310066ac077d0c3d2885410a48cf799149012e
chaud1024 <chaud1024@gmail.com>
2023-07-13 10:30:40 +0900

    #13-35 사용자 포스트 공통된 함수 병합

--- src/app/user/[username]/page.tsx
+++ src/app/user/[username]/page.tsx
@@ -1,15 +1,20 @@
 import UserPosts from "@/app/components/UserPosts";
 import UserProfile from "@/app/components/UserProfile";
 import { getUserForProfile } from "@/service/user";
+import { Metadata } from "next";
 import { notFound } from "next/navigation";
+import { cache } from "react";
 
 type Props = {
   params: { username: string };
 };
+
+const getUser = cache(async (username: string) => getUserForProfile(username));
+
 export default async function UserPage({ params: { username } }: Props) {
   // 상단: 사용자의 프로필 이미지와 정보(username, name, 숫자)
   // 하단: 3개의 탭(posts, liked, bookmarks)
-  const user = await getUserForProfile(username);
+  const user = await getUser(username);
 
   if (!user) {
     notFound();
@@ -21,4 +26,14 @@ export default async function UserPage({ params: { username } }: Props) {
       <UserPosts user={user} />
     </section>
   );
+}
+
+export async function generateMetadata({
+  params: { username },
+}: Props): Promise<Metadata> {
+  const user = await getUser(username);
+  return {
+    title: `${user?.name} (@${user?.username}) · Instagram Photos`,
+    description: `${user?.name}'s all Instagram posts`,
+  };
 }
