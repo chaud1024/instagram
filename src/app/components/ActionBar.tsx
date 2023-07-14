3bc0a89114461f8ba6a8d1264e2d1d27d7707009
chaud1024 <chaud1024@gmail.com>
2023-07-14 09:17:01 +0900

    #14-14 코멘트 - text -> children 변경

--- src/app/components/ActionBar.tsx
+++ src/app/components/ActionBar.tsx
@@ -51,12 +51,7 @@ export default function ActionBar({ post, children }: Props) {
         <p className="text-sm font-bold mb-2">{`${likes?.length ?? 0} ${
           likes?.length > 1 ? "likes" : "like"
         }`}</p>
-        {text && (
-          <p>
-            <span className="font-bold mr-1">{username}</span>
-            {text}
-          </p>
-        )}
+        {children}
         <p className="text-xs text-neutral-500 uppercase my-4 text-right">
           {paresDate(createdAt)}
         </p>
