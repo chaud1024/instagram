612e71bf44f359dca18a4be344074e01960882f5
chaud1024 <chaud1024@gmail.com>
2023-07-14 17:17:08 +0900

    #14-17 코멘트 - post.ts mutate->globalMutate 변경 및 useCallback으로 훅 리팩토링

--- src/app/hooks/post.ts
+++ src/app/hooks/post.ts
@@ -1,3 +1,4 @@
+import { useCallback } from "react";
 import useSWR, { useSWRConfig } from "swr";
 import { Comment, FullPost } from "../model/post";
 
@@ -18,21 +19,24 @@ export default function useFullPost(postId: string) {
 
   const { mutate: globalMutate } = useSWRConfig();
 
-  const postComment = (comment: Comment) => {
-    if (!post) return;
+  const postComment = useCallback(
+    (comment: Comment) => {
+      if (!post) return;
 
-    const newPost = {
-      ...post,
-      comments: [...post.comments, comment],
-    };
+      const newPost = {
+        ...post,
+        comments: [...post.comments, comment],
+      };
 
-    return mutate(addComment(post.id, comment.comment), {
-      optimisticData: newPost,
-      populateCache: false,
-      revalidate: false,
-      rollbackOnError: true,
-    }).then(() => globalMutate("/api/posts"));
-  };
+      return mutate(addComment(post.id, comment.comment), {
+        optimisticData: newPost,
+        populateCache: false,
+        revalidate: false,
+        rollbackOnError: true,
+      }).then(() => globalMutate("/api/posts"));
+    },
+    [post, mutate, globalMutate],
+  );
 
   return { post, isLoading, error, postComment };
 }
