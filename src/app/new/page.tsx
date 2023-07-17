698caef298d0c44ff9d8c8eb8767d6c5e78cc0c4
chaud1024 <chaud1024@gmail.com>
2023-07-17 17:57:49 +0900

    #14-28 새포스트 화면 ui 생성

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,3 +1,18 @@
-export default function NewPage() {
-  return <div>NewPage</div>;
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import { Metadata } from "next";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import NewPost from "../components/NewPost";
+
+export const metadata: Metadata = {
+  title: "New Post",
+  description: "Create a new post",
+};
+
+export default async function NewPage() {
+  const session = await getServerSession(authOptions);
+  if (!session?.user) {
+    redirect("/auth/signin");
+  }
+  return <NewPost user={session.user} />;
 }
